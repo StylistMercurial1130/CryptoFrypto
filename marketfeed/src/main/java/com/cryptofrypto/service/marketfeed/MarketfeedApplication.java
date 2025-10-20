ff7078ccfd5c17736ef441bfc00dd4f63a62a000
StylistMercurial1130 <wrk.krthik@gmail.com>
2025-10-20 17:19:46 +0530

    adding market feed project

---
+++ marketfeed/src/main/java/com/cryptofrypto/service/marketfeed/MarketfeedApplication.java
@@ -0,0 +1,12 @@
+package com.cryptofrypto.service.marketfeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketfeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketfeedApplication.class, args);
+	}
+}
---
+++ marketfeed/src/test/java/com/cryptofrypto/service/marketfeed/MarketfeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cryptofrypto.service.marketfeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketfeedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
